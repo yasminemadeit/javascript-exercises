bc842b6ff6633a8a4271349f26f247d6f6dfa434
Yasmine <20632722+yasminemadeit@users.noreply.github.com>
2024-10-22 18:46:55 -0400

    Pass all tests for exercise02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+	result="";
+	if (num>=0){
+		for (let i =0; i<num; i++){
+        		result+=string;
 
+		}	
+
+	}else{
+	return("ERROR")
+	}	
+
+	return result;
 };
 
 // Do not edit below this line
