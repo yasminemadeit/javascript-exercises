6bad8fdc14e47b3bb8d9888b299c18e20c65b7b4
Yasmine <20632722+yasminemadeit@users.noreply.github.com>
2024-10-23 00:17:50 -0400

    Pass all tests for exercise07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+	let celsius = ((temp-32)*5)/9;
+	return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+	let fahrenheit = 32+ (temp * (9/5));
+	return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
