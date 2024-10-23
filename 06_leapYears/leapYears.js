1d58c5b3749a415c9d4336a75585faa1a47867aa
Yasmine <20632722+yasminemadeit@users.noreply.github.com>
2024-10-22 21:31:18 -0400

    Pass all tests for exercise06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+	 leap=false;
+    if (year%4==0){
+        if (year %100==0 && year %400==0){
+            leap = true;
+        }if (year%100!=0){
+            leap=true;
+        }
+    }
+    return leap;
+  
 };
 
 // Do not edit below this line
