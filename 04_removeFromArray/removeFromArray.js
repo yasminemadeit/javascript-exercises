a86e190373f700e6c618c3ac15fa90e037debcab
Yasmine <20632722+yasminemadeit@users.noreply.github.com>
2024-10-22 20:38:35 -0400

    Pass all tests for exercise04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+	const argsArray = Array.from(args);
+	for(let i=array.length-1; i>=0; i--){
+          for (let j=0; j<argsArray.length; j++){
+              if (array[i]===argsArray[j]){
+                  array.splice(i,1);
+              }
+          }
+      }
+      
+     
+      return array;
 };
 
 // Do not edit below this line
