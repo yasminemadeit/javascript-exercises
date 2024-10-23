032dd6ae8db65796389d01fb2ff267fbadf1b724
Yasmine <20632722+yasminemadeit@users.noreply.github.com>
2024-10-22 21:15:07 -0400

    Pass all tests for exercise05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+	let sum = 0;
+	let max;
+	let min;
+	if (Number.isInteger(num1) && Number.isInteger(num2) && num1 > 0 && num2 > 0){
+	(num1>num2)? (max=num1, min=num2): (max=num2, min=num1);
+        for (let i =min; i<=max; i++){
 
+        sum+=i;
+
+        }
+        return sum;
+	}else{
+
+	return("ERROR");
+
+	}
 };
 
 // Do not edit below this line
